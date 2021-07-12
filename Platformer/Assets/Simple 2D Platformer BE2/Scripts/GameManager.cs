465ab5129c3df444ff7e43c371edec0858f1f672
june020503 <june020503@gmail.com>
2021-07-13 04:08:26 +0900

    UI (사운드 제외)

--- Platformer/Assets/Simple 2D Platformer BE2/Scripts/GameManager.cs
+++ Platformer/Assets/Simple 2D Platformer BE2/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +14,18 @@ public class GameManager : MonoBehaviour
     public PlayerMove player;
     public GameObject[] Stages;
 
+    public Image UIhealth;
+    public Text Hp;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject RestartBtn;
+
+    private void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+        Hp.text = Health.ToString();
+    }
+
     public void NextStage()
     {
         //스테이지 이동
@@ -20,12 +34,19 @@ public class GameManager : MonoBehaviour
             Stages[stageIndex].SetActive(false);
             stageIndex++;
             Stages[stageIndex].SetActive(true);
+
+            UIStage.text = "STAGE" + (stageIndex + 1);
         }
         
         else
         {
             //게임 클리어
             Time.timeScale = 0;
+
+            //재시작 버튼 UI
+            Text btnText = RestartBtn.GetComponentInChildren<Text>();
+            btnText.text = "Game Clear!";
+            RestartBtn.SetActive(true);
         }
 
         //플레이어 위치 초기화
@@ -34,6 +55,8 @@ public class GameManager : MonoBehaviour
         //점수 합산
         totalPoint += stagePoint;
         stagePoint = 0;
+
+        Health++;
     }
 
     public void HealthDown()
@@ -47,6 +70,8 @@ public class GameManager : MonoBehaviour
         else
         {
             player.onDie();
+
+            RestartBtn.SetActive(true);
         }
     }
 
@@ -70,5 +95,11 @@ public class GameManager : MonoBehaviour
     {
         player.transform.position = new Vector3(0, 0, -1);
         player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
